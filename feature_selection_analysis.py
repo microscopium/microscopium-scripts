e92d46373d5123e4761b421060282657b67ecf19
sahas <susmi06@yahoo.com>
2015-02-09 13:40:52 +1100

    feature_selection_analysis script
    Hopefully I have correctly uploaded the script
    this time.

--- feature_selection_analysis.py
+++ feature_selection_analysis.py
@@ -21,7 +21,7 @@ coll_name : string
         The name of the collection within the database
         database_name = 'myofusion' for big dataset
         collection_name = 'wells'
- mongoconnect for big_data: mongod --dbpath ~/project/husc/tests/testdata/big_data/mongodb/
+ mongoconnect: mongod --dbpath ~/project/husc/tests/testdata/big_data/mongodb/
  """
 
 
