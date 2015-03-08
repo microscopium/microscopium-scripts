6faa12d123346b67ab2cf8887d3e8dffcbff0567
Adrian Hecker <starcalibre@gmail.com>
2015-03-08 14:40:00 +1100

    Update channel_order argument

--- ht29-stitch.py
+++ ht29-stitch.py
@@ -18,7 +18,7 @@ tif_files = pathutils.all_matching_files(tif_out_dir, glob='*.tif', case_sensiti
 cellomics.batch_stitch_stack(file_dict=cellomics.make_key2file(tif_files),
                              output='/home/starcalibre/NIRHTa-001-tif-stitch',
                              stitch_order=[[2, 3, 4], [1, 0, 5]],
-                             channel_map=[1, 2, 0],
+                             channel_order=[1, 2, 0],
                              target_bit_depth=8)
 
 
