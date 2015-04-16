b1c5c9a341c121e8fb73f9a78966d92f630a6b0d
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2015-04-16 05:39:40 +0000

    Bug fix: change directory to process tiffs
    Additionally, fix echo print calls.

--- process-bbbc017.sh
+++ process-bbbc017.sh
@@ -3,16 +3,18 @@
 infile=$1
 outdir=$2
 
-echo ====================\nUNZIPPING\n====================
+echo -e "====================\nUNZIPPING\n===================="
 unzip $infile -d $outdir
 
-echo ====================\nCONVERTING\n====================
+echo -e "====================\nCONVERTING\n===================="
 source activate cellom
 created_name=`python -c "import sys; print(sys.argv[1].rsplit('_')[-1].rsplit('.')[0])" $infile`
+datadir=${created_name}-tif
 cd $outdir
-cellom2tif -c 2 $created_name ${created_name}-tif
+cellom2tif -c 2 $created_name $datadir
 
-echo ====================\nILLUMINATION\n====================
+echo -e "====================\nILLUMINATION\n===================="
+cd $datadir
 source activate mic3
 for channel in d0 d1 d2; do
     for field in f00 f01 f02 f03 f04 f05; do
@@ -22,5 +24,5 @@ for channel in d0 d1 d2; do
     done
 done
 
-echo ====================\nSTITCHING\n====================
+echo -e "====================\nSTITCHING\n===================="
 mic montage *.illum.tif -c 2 -o "[[2, 3, 4], [1, 0, 5]]" -O "[1, 2, 0]"
